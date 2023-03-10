70cca9d0d3dcc59ebef781ea4cf52321c2409cae
anabang <anapoulzrn@gmail.com>
2023-03-10 22:43:13 +0600

    hw8

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { TodoList } from "./components/todo-list";
+import { AddTodo } from "./components/add-todo";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <AddTodo />
+        <TodoList />
       </header>
     </div>
   );
